345d5778d9c69d28b56e9e0c0aff8f17320bc3ed
Grzegorz Cieslik <grzegorz.cieslik@hotmail.com>
2021-01-05 21:10:15 +0100

    Added server connection and database.
     On branch signin
     Changes to be committed:
    	modified:   src/App.js
    	modified:   src/components/faceRecognition/FaceRecognition.js
    	modified:   src/components/imageLinkForm/ImageLinkForm.js
    	modified:   src/components/logo/Logo.js
    	modified:   src/components/navigation/Navigation.js
    	modified:   src/components/rank/Rank.js
    	new file:   src/components/register/Register.js
    	new file:   src/components/signIn/SignIn.js

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             </p>
             <div className='center'>
                 <div className='center form pa1 br3 shadow-5'>
-                    <input className='f4 pa1 w-70 center' type='text' onChange={onInputChange} />
+                    <input className='f4 pa1 w-70 center' type='text' placeholder='Your image URL:' onChange={onInputChange} />
                     <button className='w-30 grow f4 link ph3 pv 2 dib white bg-light-blue' onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>

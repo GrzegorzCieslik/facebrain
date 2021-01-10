5e97c9f8ab2c801d498a9ce78ede8549e75dc4cd
Grzegorz Cieslik <grzegorz.cieslik@hotmail.com>
2021-01-10 09:49:07 +0100

    Some security changes.
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js
    	modified:   src/components/register/Register.js

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -24,8 +24,8 @@ class Register extends React.Component {
 
   onSubmitSignIn = () => {
     if (this.state.signInEmail || this.state.signInPassword || this.state.name === '') {
-      alert("Please Fill All Required Fields")
-    } else {
+      return alert("Please fill all required fields")
+    }
     fetch('http://localhost:3000/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
@@ -37,12 +37,11 @@ class Register extends React.Component {
     })
       .then(response => response.json())
       .then(user => {
-        if (user) {
+        if (user.id) {
           this.props.loadUser(user)
           this.props.onRouteChange('home');
         }
       })
-    }
   }
 
   render() {
